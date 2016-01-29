60898526f90d1808ea40a0987ede9860a13b69ac
Danny van Kooten <dannyvankooten@gmail.com>
2016-01-29 15:25:34 +0700

    Make sure `$args` is optional when calling `wpkb_table_of_contents`. Allow passing an empty string as `title` to disable it.

--- src/table-of-contents/functions.php
+++ src/table-of-contents/functions.php
@@ -5,7 +5,7 @@
  *
  * @return string
  */
-function wpkb_table_of_contents( $args ) {
+function wpkb_table_of_contents( $args = array() ) {
 	global $post;
 
 	$content = '';
@@ -18,19 +18,25 @@ function wpkb_table_of_contents( $args ) {
 	// parse content for <h2> elements with an ID
 	preg_match_all( '/<h2 id="([a-z\-]+)">([a-zA-Z0-9\s]+)<\/h2>/i', $post->post_content, $headings );
 
-	if( ! empty( $headings[1] ) ) {
-		$content .= '<div class="wpkb-table-of-contents">';
-		$content .= '<h4>' . $args['title'] . '</h4>';
-
-		$content .= '<ul>';
-		foreach( $headings[1] as $i => $heading_id ) {
-			$heading_text = $headings[2][$i];
-			$content .= sprintf( '<li><a href="#%s">%s</a></li>', $heading_id, $heading_text );
-
-		}
-		$content .= '</ul>';
-		$content .= '</div>';
+	if( empty( $headings[1] ) ) {
+		return '';
 	}
+
+	$content .= '<div class="wpkb-table-of-contents">';
+
+	// only add title if it's not explicitly passed as empty
+	if( ! empty( $args['title'] ) ) {
+		$content .= '<h4>' . $args['title'] . '</h4>';
+	}
+
+	$content .= '<ul>';
+	foreach( $headings[1] as $i => $heading_id ) {
+		$heading_text = $headings[2][$i];
+		$content .= sprintf( '<li><a href="#%s">%s</a></li>', $heading_id, $heading_text );
+
+	}
+	$content .= '</ul>';
+	$content .= '</div>';
 
 	return $content;
 }
